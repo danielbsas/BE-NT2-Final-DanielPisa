8f5ac6561c481949747d9a36ab431757569d279c
Danielbsas <danielpisa@gmail.com>
2022-07-12 20:44:41 -0300

    BE-NT2 Examen Final, Daniel Pisa

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Final />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Final from './components/final.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Final
   }
 }
 </script>
---
+++ src/components/final.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="src-components-final">
+    <h1>Final Daniel Pisa</h1>
+    <div>
+      <span> Color: <input type="text" v-model="color" /> </span>
+      <span> Fondo: <input type="text" v-model="fondo" /> </span>
+      <span> Size: <input type="text" v-model="size" /> </span>
+    </div>
+    <hr />
+    <input type="text" v-model.trim="texto" :style="cambiarEstilo()" />
+    
+    <p>Cantidad de palabras: {{ contarPalabras }}</p>
+
+    <hr />
+<table class="table">
+        <div class="table-responsive">
+          <table class="table table-dark">
+            <tr>
+              <th>Nro pregunta</th>
+              <th>Respuesta</th>
+            </tr>
+            <tr v-for="(pregunta,index) in preguntas" :key="index" :style="{color: 'black', backgroundColor:'silver'}">
+              <td>{{ pregunta.numero }}</td>
+              <td>{{ pregunta.respuesta }}</td>
+            </tr>
+          </table>
+       </div>
+      </table>
+  </section>
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-final',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        color: '',
+        fondo: '',
+        size: '',
+        texto: '',
+        preguntas : [
+                    { numero: 1, respuesta: ['B'] },
+                    { numero: 2, respuesta: ['C'] },
+                    { numero: 3, respuesta: ['B'] },
+                    { numero: 4, respuesta: ['D'] },
+                    { numero: 5, respuesta: ['D'] },
+                    ]
+      
+      }
+    },
+    methods: {
+cambiarEstilo() {
+        return {
+          color: this.color,
+          backgroundColor: this.fondo,
+          fontSize: this.size + "px"
+        };
+    }
+    },
+    computed: {
+    contarPalabras() {
+      let cant = this.texto.split(' ').length;
+		return this.texto == "" ? 0 : cant
+	}
+}
+}
+</script>
+
+<style scoped lang="css"></style>
